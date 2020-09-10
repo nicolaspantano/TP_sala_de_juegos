a44789ad847f161b9a8131fbfcbca4c45d5441e0
Nicolas <nicolaspantano757@gmail.com>
2020-09-10 10:21:32 -0300

    ruteo

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RutaUnoComponent } from './components/ruta-uno/ruta-uno.component';
+import { RutaDosComponent } from './components/ruta-dos/ruta-dos.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RutaUnoComponent,
+    RutaDosComponent,
+    HomeComponent,
+    MenuComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
